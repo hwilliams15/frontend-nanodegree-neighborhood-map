a4e3db326d1a393b3a1f6412ba6a7e34bedca165
Holly Williams <hlly.williams@gmail.com>
2016-10-26 04:07:52 -0400

    Used Google StreetView instead of flickr, cleaned up viewmodel and css

--- js/main.js
+++ js/main.js
@@ -1,276 +1,336 @@
 function AppViewModel() {
-    self = this;
-    self.filterText = ko.observable();
-    self.interestingPoints = ko.observableArray([
-      {
-        'title':'Central Park',
-        'location': {'lat': 40.782824,'lng': -73.965570},
-        'id':0
-      },
-      {
-        'title':'Metropolitan Opera',
-        'location': {'lat': 40.773006,'lng': -73.984148},
-        'id': 1
-      },{
-        'title':'Times Square',
-        'location': {'lat': 40.759155,'lng': -73.985136},
-        'id': 2
-      },{
-        'title':'New York Stock Exchange',
-        'location': {'lat': 40.707097,'lng': -74.011260},
-        'id': 3
-      },{
-        'title':'United Nations Headquarters',
-        'wikiTitle': 'Headquarters of the United Nations',
-        'location': {'lat': 40.749128,'lng': -73.967971},
-        'id': 4
-      },{
-        'title':'Madame Tussauds New York',
-        'location': {'lat': 40.756976,'lng': -73.988831},
-        'id': 5
-      },{
-        'title':'Bronx Zoo',
-        'location': {'lat': 40.850530,'lng': -73.876692},
-        'id': 6
-      },{
-        'title':'Metropolitan Museum of Art',
-        'location': {'lat': 40.779705,'lng': -73.963260},
-        'id': 7
-      },{
-        'title': 'Ground Zero',
-        'wikiTitle':'World Trade Center site',
-        'location': {'lat': 40.712224,'lng': -74.010413},
-        'id': 8
-      },{
-        'title':'Empire State Building',
-        'location': {'lat': 40.748701,'lng': -73.985627},
-        'id': 9
-      },{
-        'title':'Yankee Stadium',
-        'location': {'lat': 40.829707,'lng': -73.925697},
-        'id': 10
-      }
-    ]);
+  var self = this;
+  self.filterText = ko.observable();
+  self.hamburger = $('.hamburger');
+  self.listView = $('.list-view');
+  self.rightContent = $('.right');
+  self.mobileFilter = $('.mobile-filter-input-group');
+  self.mobileCenterMapButton = $('.right .center-map-button');
+  self.listCenterMapButton = $('.list-view .center-map-button');
 
-    self.filterLocations = function(){
-      self.infoWindow.close();
-      var listElems = $('.list-elem');
-      var list = self.interestingPoints();
-      for(var i = 0;i<list.length;i++){
-        if(list[i].title.toLowerCase().indexOf(self.filterText().toLowerCase())<0){
-          listElems[i].style.display = "none";
-          list[i].marker.setMap(null);
-        }else{
-          listElems[i].style.display = "block";
-          list[i].marker.setMap(map);
+  self.interestingPoints = ko.observableArray([{
+    'title': 'Central Park',
+    'location': {
+      'lat': 40.782824,
+      'lng': -73.965570
+    },
+    'id': 0
+  }, {
+    'title': 'Metropolitan Opera',
+    'location': {
+      'lat': 40.773006,
+      'lng': -73.984148
+    },
+    'id': 1
+  }, {
+    'title': 'Times Square',
+    'location': {
+      'lat': 40.759155,
+      'lng': -73.985136
+    },
+    'id': 2
+  }, {
+    'title': 'New York Stock Exchange',
+    'location': {
+      'lat': 40.707097,
+      'lng': -74.011260
+    },
+    'id': 3
+  }, {
+    'title': 'United Nations Headquarters',
+    'wikiTitle': 'Headquarters of the United Nations',
+    'location': {
+      'lat': 40.7494561,
+      'lng': -73.9693231
+    },
+    'id': 4
+  }, {
+    'title': 'Statue of Liberty',
+    'location': {
+      'lat': 40.689201,
+      'lng': -74.043824
+    },
+    'id': 5
+  }, {
+    'title': 'Grand Central Terminal',
+    'location': {
+      'lat': 40.7521563,
+      'lng': -73.9777261
+    },
+
+    'id': 6
+  }, {
+    'title': 'Metropolitan Museum of Art',
+    'location': {
+      'lat': 40.779705,
+      'lng': -73.963260
+    },
+    'id': 7
+  }, {
+    'title': 'Ground Zero',
+    'wikiTitle': 'World Trade Center site',
+    'location': {
+      'lat': 40.7122,
+      'lng': -74.0104
+    },
+    'id': 8
+  }, {
+    'title': 'Empire State Building',
+    'location': {
+      'lat': 40.748701,
+      'lng': -73.985627
+    },
+    'id': 9
+  }, {
+    'title': 'Yankee Stadium',
+    'location': {
+      'lat': 40.829707,
+      'lng': -73.925697
+    },
+    'id': 10
+  }]);
+
+  /*
+  Decide which locations should be visible based on the filter text.
+  Also filter the markers on the map.
+  */
+  self.filterLocations = function() {
+    self.infoWindow.close();
+    var listElems = $('.list-elem');
+    var list = self.interestingPoints();
+    for (var i = 0; i < list.length; i++) {
+      if (list[i].title.toLowerCase().indexOf(self.filterText().toLowerCase()) < 0) {
+        listElems[i].style.display = 'none';
+        list[i].marker.setMap(null);
+      } else {
+        listElems[i].style.display = 'block';
+        list[i].marker.setMap(self.map);
+      }
+    }
+  };
+
+  /*
+  Animate the marker and display the info for the location
+  */
+  self.displayInfoWindow = function(location) {
+    location.marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function() {
+      location.marker.setAnimation(null);
+    }, 750);
+    self.populateInfoWindow(location, self.infoWindow);
+    self.adjustListView();
+  };
+
+  /*
+  Create the markers for the view model's list of locations
+  */
+  self.createMarkers = function(locations) {
+
+    var bounds = new google.maps.LatLngBounds();
+    self.infoWindow = new google.maps.InfoWindow({
+      content: 'This is an info window.'
+    });
+
+    //clear the marker on close
+    self.infoWindow.addListener('closeclick', function() {
+      self.infoWindow.marker = null;
+    });
+
+    //close the info window if the user clicks on the map
+    self.map.addListener('click', function() {
+      if (self.infoWindow.map != null) {
+        self.infoWindow.marker = null;
+        self.infoWindow.close();
+      }
+    });
+
+    var markerImage = new google.maps.MarkerImage(
+      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + 'FFFF24' +
+      '|40|_|%E2%80%A2',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0, 0),
+      new google.maps.Point(10, 34),
+      new google.maps.Size(21, 34));
+
+    for (var i = 0; i < locations.length; i++) {
+      var position = locations[i].location;
+      var title = locations[i].title;
+
+      var marker = new google.maps.Marker({
+        position: position,
+        title: title,
+        animation: google.maps.Animation.DROP,
+        id: i,
+        map: self.map,
+        icon: markerImage
+      });
+
+      marker.location = locations[i];
+
+      //when the marker is clicked animate with bounce
+      //and stop the bouncing animation later
+      marker.addListener('click', function() {
+        this.setAnimation(google.maps.Animation.BOUNCE);
+        (function(marker) {
+          setTimeout(function() {
+            marker.setAnimation(null);
+          }, 750);
+        })(this);
+        self.populateInfoWindow(this.location);
+      });
+
+      locations[i].marker = marker;
+
+      bounds.extend(marker.position);
+    }
+    self.bounds = bounds;
+    self.map.fitBounds(bounds);
+  };
+
+  self.populateInfoWindow = function(location) {
+    var marker = location.marker;
+    if (self.infoWindow.marker != marker) {
+      // Clear the infoWindow content to give the streetview time to load.
+      self.infoWindow.setContent('');
+      self.infoWindow.marker = marker;
+
+
+      var content = '<div id="title">' + location.title + '</div>' +
+        '<div id="pano">No Street View Found</div>' +
+        '<div id="wiki-content"></div>';
+
+      self.infoWindow.setContent(content);
+      self.infoWindow.open(self.map, marker);
+
+      self.retrieveWikiInfo(location);
+      self.getPanoramaView(marker);
+
+    }
+  };
+
+  //send a request to retrieve the Wikipedia information and add it to the info window
+  self.retrieveWikiInfo = function(location) {
+    if (!location.wikiContent) {
+      var queryURL = self.createWikipediaUrl(location);
+      $.ajax({
+        url: queryURL,
+        dataType: 'jsonp',
+        type: 'POST',
+        headers: {
+          'Api-User-Agent': 'Example/1.0'
+        },
+        success: function(data) {
+          var id = Object.keys(data.query.pages)[0];
+          location.wikiContent = '<div>' + location.title + '</div><div id="pano"></div>';
+          if (id != null) {
+            var summary = data.query.pages[id].extract;
+            if (summary != null) {
+              location.wikiContent = summary;
+              $('#wiki-content').text(location.wikiContent);
+
+            }
+          }
+        },
+        error: function() {
+          location.wikiContent = 'Unable to load Wikipedia content.';
+          $('#wiki-content').text(location.wikiContent);
+
         }
+      });
+    } else {
+      $('#wiki-content').text(location.wikiContent);
+    }
+  };
+
+  //creates the Wikipedia URL based on the location's information
+  self.createWikipediaUrl = function(location) {
+    var title = location.wikiTitle;
+    if (!title) {
+      title = location.title;
+    }
+
+    //get two sentences from the pages intro
+    var queryURL = 'https://en.wikipedia.org/w/api.php?action=query&titles=' + title +
+      '&prop=extracts|images&imlimit=2&exintro=&explaintext=&exsentences=2&format=json';
+    return queryURL;
+  };
+
+  //Use the Google Street View Service to get a panorama to display
+  self.getPanoramaView = function(marker) {
+
+    var streetViewService = new google.maps.StreetViewService();
+    var radius = 50;
+
+
+    function getStreetView(data, status) {
+      if (status == google.maps.StreetViewStatus.OK) {
+        var nearStreetViewLocation = data.location.latLng;
+        var heading = google.maps.geometry.spherical.computeHeading(
+          nearStreetViewLocation, marker.position);
+        var panoramaOptions = {
+          position: nearStreetViewLocation,
+          pov: {
+            heading: heading,
+            pitch: 30
+          },
+          visible: true
+        };
+
+        //place in 'pano' div
+        new google.maps.StreetViewPanorama(
+          document.getElementById('pano'), panoramaOptions);
+      } else {
+        $('#pano').text('No Street View Found');
       }
-    };
+    }
+    streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+  };
 
-    self.displayInfoWindow = function(location){
-      location.marker.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function(){
-        location.marker.setAnimation(null);
-      },750);
-      populateInfoWindow(location, self.infoWindow);
-    };
+  /*
+  Slide the left handed list view as needed
+  */
+  self.adjustListView = function() {
+    self.listView.toggleClass('open');
+    self.rightContent.toggleClass('minimized');
+    self.mobileFilter.toggleClass('visible');
+  };
 
+  /*
+  Recenter the map to fit the markers
+  */
+  self.fitBounds = function() {
+    self.map.fitBounds(self.bounds);
+  };
+
+  self.hamburger.on('click', self.adjustListView);
+  self.mobileCenterMapButton.on('click', self.fitBounds);
+
+  self.listCenterMapButton.on('click', function() {
+    self.fitBounds();
+    self.adjustListView();
+  });
 }
 
 // Activates knockout.js
 var viewModel = new AppViewModel();
 ko.applyBindings(viewModel);
 
-var map;
-var markers;
 function initMap() {
-  // Constructor creates a new map - only center and zoom are required.
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.7413549, lng: -73.9980244},
-    zoom: 13,
+
+  viewModel.map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 40.782824,
+      lng: -73.965570
+    },
+    zoom: 25,
     styles: styles
   });
 
-  createMarkers(viewModel.interestingPoints(),map);
+  viewModel.createMarkers(viewModel.interestingPoints());
 }
 
-function createMarkers(locations,map){
-
-  var bounds = new google.maps.LatLngBounds();
-  viewModel.infoWindow = new google.maps.InfoWindow({
-    content: 'This is an info window.'
-  });
-
-  var markerImage = new google.maps.MarkerImage(
-    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ 'FFFF24'+
-    '|40|_|%E2%80%A2',
-    new google.maps.Size(21, 34),
-    new google.maps.Point(0, 0),
-    new google.maps.Point(10, 34),
-    new google.maps.Size(21,34));
-
-  for (var i = 0; i < locations.length; i++) {
-    // Get the position from the location array.
-    var position = locations[i].location;
-    var title = locations[i].title;
-    // Create a marker per location, and put into markers array.
-    var marker = new google.maps.Marker({
-      position: position,
-      title: title,
-      animation: google.maps.Animation.DROP,
-      id: i,
-      map: map,
-      icon: markerImage
-    });
-
-    marker.location = locations[i];
-
-    marker.addListener('click', function() {
-      this.setAnimation(google.maps.Animation.BOUNCE);
-      (function(marker){
-        setTimeout(function(){
-          marker.setAnimation(null);
-        },750);
-      })(this);
-      populateInfoWindow(this.location, viewModel.infoWindow);
-    });
-
-    locations[i].marker = marker;
-
-    bounds.extend(marker.position);
-  }
-  map.fitBounds(bounds);
-}
-
-function populateInfoWindow(location, infoWindow) {
-  var marker = location.marker;
-  if (infoWindow.marker != marker) {
-    // Clear the infoWindow content to give the streetview time to load.
-    infoWindow.setContent('');
-    infoWindow.marker = marker;
-    // Make sure the marker property is cleared if the infoWindow is closed.
-    infoWindow.addListener('closeclick', function() {
-      infoWindow.marker = null;
-    });
-    var streetViewService = new google.maps.StreetViewService();
-    var radius = 50;
-    // In case the status is OK, which means the pano was found, compute the
-    // position of the streetview image, then calculate the heading, then get a
-    // panorama from that and set the options
-    function getStreetView(data, status) {
-      if (status == google.maps.StreetViewStatus.OK) {
-        var nearStreetViewLocation = data.location.latLng;
-        var heading = google.maps.geometry.spherical.computeHeading(
-          nearStreetViewLocation, marker.position);
-          // infoWindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-          var panoramaOptions = {
-            position: nearStreetViewLocation,
-            pov: {
-              heading: heading,
-              pitch: 30
-            }
-          };
-        var panorama = new google.maps.StreetViewPanorama(
-          document.getElementById('pano'), panoramaOptions);
-      } else {
-        $("#pano").html("<div>No Street View Found</div>");
-      }
-    }
-    // Use streetview service to get the closest streetview image within
-    // 50 meters of the markers position
-    streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-    // Open the infoWindow on the correct marker.
-    // retrieveWikiInfo(location,infoWindow);
-    getFlickerInfo(location,infoWindow);
-    infoWindow.open(map, marker);
-  }
-}
-function createWikipediaUrl(location){
-  var title = location.wikiTitle;
-  if(!title){
-    title = location.title;
-  }
-  var queryURL = "https://en.wikipedia.org/w/api.php?action=query&titles="+title+
-  "&prop=extracts|images&imlimit=2&exintro=&explaintext=&exsentences=2&format=json";
-  return queryURL;
-}
-
-function retrieveWikiInfo(location,infoWindow){
-  if(!location.wikiContent){
-    var queryURL = createWikipediaUrl(location);
-    console.log(queryURL);
-    $.ajax( {
-        url: queryURL,
-        dataType: 'jsonp',
-        type: 'POST',
-        headers: { 'Api-User-Agent': 'Example/1.0' },
-        success: function(data) {
-          var id = Object.keys(data.query.pages)[0];
-          location.wikiContent = '<div>' + location.title + '</div><div id="pano"></div>';
-          if(id!=null){
-            var summary = data.query.pages[id].extract;
-            if(summary!=null){
-               location.wikiContent = '<div>' + location.title + '</div>'+
-               '<div id="pano"></div>'+
-               '<div>'+summary+'</div>';
-               infoWindow.setContent(location.wikiContent);
-            }
-          }
-        },
-        error: function(){
-          location.wikiContent = '<div>' + marker.title + '</div>';
-            '<div id="pano"></div>'+
-            '<div>Unable to load Wikipedia content</div>';
-          infoWindow.setContent(location.wikiContent);
-        }
-    } );
-  }
-  else{
-    infoWindow.setContent(location.wikiContent);
-  }
-}
-
-var hamburger = $('.hamburger');
-var listView = $('.list-view');
-var rightContent = $('.right');
-var mobileFilter = $('.mobile-filter-input-group');
-
-hamburger.on("click",function(){
-  listView.toggleClass('open');
-  rightContent.toggleClass('minimized');
-  mobileFilter.toggleClass('visible');
-});
-
-function getFlickerInfo(location,infoWindow){
-
-  var queryTag = location.title;
-  var flickerURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search"
-  +"&api_key=8343a630cdd7b965cf0cf265fb93fe0b&tags="+queryTag+"&per_page=10&page=1&"+
-  "format=json&nojsoncallback=1";
-
-  $.ajax( {
-      url: flickerURL,
-      dataType: 'json',
-      type: 'POST',
-      success: function(data) {
-        var imgSrc = getImageURL(data);
-        infoWindow.setContent("<img src="+imgSrc+" style='width:200px;'>");
-      },
-      error: function(){
-        location.wikiContent = '<div>' + marker.title + '</div>';
-          '<div id="pano"></div>'+
-          '<div>Unable to load Wikipedia content</div>';
-        infoWindow.setContent(location.wikiContent);
-      }
-  } );
-
-}
-function getImageURL(data){
-  var photo = data.photos.photo[0];
-  var farm = photo.farm;
-  var server = photo.server;
-  var id = photo.id;
-  var secret=photo.secret;
-  var imageURL = "https://farm"+farm+".staticflickr.com/"+server+"/"+id+"_"+secret+".jpg";
-  return imageURL;
+function googleError() {
+  var map = $('#map');
+  map.addClass('error');
+  map.html('<table class="error"><tr><td>Error loading neighborhood map.</td></tr></table>');
 }
